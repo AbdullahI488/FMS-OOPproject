f17e9a25ddc1c2f0f1813cd0ad5310017714e499
l250709 <l250709@lhr.nu.edu.pk>
2026-05-10 20:33:34 +0500

    added node definitions

--- src/Node.h
+++ src/Node.h
@@ -1,8 +1,22 @@
 #ifndef NODE_H
 #define NODE_H
 #include<iostream>
-using namespace std;
+class Folder;
 class Node {
+protected:
+	std::string name;
+	std::string pathname;
+	Folder* ptr;
+public:
+	Node(std::string n, std::string p, Folder* ptr);
+	std::string GetName();
+	std::string GetPath();
+	Folder* GetParent();
+	virtual ~Node();
+	virtual std::string GetType();
+
+
 
 };
+
 #endif
