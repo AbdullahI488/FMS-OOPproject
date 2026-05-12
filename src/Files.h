322b4d4ff41cf530048b8759cb12c79a8de1c321
AbdullahI488 <155844680+AbdullahI488@users.noreply.github.com>
2026-05-12 16:31:34 +0500

    Define File class with methods and destructor
    Added File class with constructors and virtual methods.

--- src/Files.h
+++ src/Files.h
@@ -1,9 +1,21 @@
 #ifndef FILES_H
 #define FILES_H
 #include<iostream>
-using namespace std;
 #include "Node.h"
 class File {
+public:
+
+	File(std::string name, std::string path, Folder* ptr);
+
+	long long getsize() const override;
+
+	void deletefromdisk() override;
+
+	virtual void open() = 0;
+
+	virtual std::string gettype() const = 0;
+
+	virtual ~File() = default;
 
 };
 #endif
