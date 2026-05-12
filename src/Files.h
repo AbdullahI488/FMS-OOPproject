8db7f02082947842f75286864318808dcfe63c6a
AbdullahI488 <155844680+AbdullahI488@users.noreply.github.com>
2026-05-12 23:43:50 +0500

    Refactor File class and update header guards

--- src/Files.h
+++ src/Files.h
@@ -1,36 +1,21 @@
-#ifndef FILE_H
-#define FILE_H
+#ifndef FILES_H
+#define FILES_H
 
+#include "Node.h"
 #include <string>
 
-class Folder;
-
-class File {
-protected:
-    std::string name;
-    std::string diskPath;
-    Folder* parent;
-
+class File : public Node {
 public:
     File(const std::string& name,
-        const std::string& diskPath,
-        Folder* parent)
-        : name(name), diskPath(diskPath), parent(parent) {
+         const std::string& diskPath,
+         Folder* parent)
+        : Node(name, diskPath, parent) {
     }
 
     virtual ~File() = default;
 
-    const std::string& getName() const { return name; }
-    const std::string& getDiskPath() const { return diskPath; }
-    Folder* getParent() const { return parent; }
-
-    virtual void createOnDisk()=0;
-    virtual void open() = 0;
-    virtual std::string getType() const = 0;
-    virtual bool deleteFromDisk() = 0;
-    virtual void loadFromDisk()=0;
-     virtual void saveToDisk()=0;
-     virtual void runEditor() = 0;
+    virtual long long getsize() const override;
+    virtual bool deleteFromDisk() override;
 };
 
 #endif
