1ed997b3504ee6e6c109390777937772f247e52c
AbdullahI488 <155844680+AbdullahI488@users.noreply.github.com>
2026-05-12 23:44:24 +0500

    Refactor Folder class and add search functionality

--- src/Folder.cpp
+++ src/Folder.cpp
@@ -1,110 +1,118 @@
 #include "Folder.h"
 #include <iostream>
-#include <filesystem> //for actual file making
+#include <filesystem>//for actual filemaking
 
 namespace fs = std::filesystem;
 
 Folder::Folder(std::string name, std::string path, Folder* ptr)
-	: Node(name, path, ptr) {
+    : Node(name, path, ptr) {
+    space = new Node*[5];
+    count = 0;
+    capacity = 5;
 
-	space = new Node * [5];
-	count = 0;
-	capacity = 5;
-
-	try {
-		fs::create_directories(path);
-	}
-
-	catch (...) {
-		std::cout << "folder could not be created\n";
-	}
+    try {
+        fs::create_directories(path);
+    }
+    catch (...) {
+        std::cout << "folder could not be created\n";
+    }
 }
 
+void Folder::resize() {
+    int newcapacity = capacity * 2;
+    Node** newspace = new Node*[newcapacity];
 
-void Folder::resize() { // for if the folder size is too small
-	int newcapacity = capacity * 2;
-  
-	Node** newspace = new Node * [newcapacity];
-  
-	for (int i = 0; i < count; i++) {
-		newspace[i] = space[i];
-	}
+    for (int i = 0; i < count; i++) {
+        newspace[i] = space[i];
+    }
 
-	delete[] space;
+    delete[] space;
 
-	space = newspace;
-	capacity = newcapacity;
+    space = newspace;
+    capacity = newcapacity;
 }
 
 void Folder::addchild(Node* node) {
-
-	if (count == capacity) {
-		resize();
-	}
-
-	space[count] = node;
-	count++;
+    if (count == capacity) {
+        resize();
+    }
+    space[count++] = node;
 }
 
-bool Folder::removechild(std::string name) {
-
-	for (int i = 0; i < count; i++) {
-		if (space[i]->getname() == name) {
-
-			for (int j = i; j < count - 1; j++) {
-				space[j] = space[j + 1];
-			}
-
-			count--;
-			return true;
-		}
-	}
-
-	return false;
+bool Folder::removechild(const std::string& name) {
+    for (int i = 0; i < count; i++) {
+        if (space[i]->getname() == name) {
+            for (int j = i; j < count - 1; j++) {
+                space[j] = space[j + 1];
+            }
+            count--;
+            return true;
+        }
+    }
+    return false;
 }
 
-Node* Folder::findchild(std::string name) const {
+Node* Folder::findchild(const std::string& name) const {
+    for (int i = 0; i < count; i++) {
+        if (space[i]->getname() == name) {
+            return space[i];
+        }
+    }
+    return nullptr;
+}
 
-	for (int i = 0; i < count; i++) {
+std::string Folder::searchRecursive(const std::string& targetname) const {
+    if (getname() == targetname) {
+        return getdiskpath();
+    }
 
-		if (space[i]->getname() == name) {
+    for (int i = 0; i < count; i++) {
+        if (space[i]->getname() == targetname) {
+            return space[i]->getdiskpath();
+        }
 
-			return space[i];
-		}
-	}
+        Folder* folder = dynamic_cast<Folder*>(space[i]);
+        if (folder != nullptr) {
+            std::string result = folder->searchRecursive(targetname);
+            if (!result.empty()) {
+                return result;
+            }
+        }
+    }
 
-	return nullptr;
+    return "";
 }
 
 long long Folder::getsize() const {
+    long long total = 0;
 
-	long long total = 0;
+    for (int i = 0; i < count; i++) {
+        total += space[i]->getsize();
+    }
 
-	for (int i = 0; i < count; i++) {
-		total = total + space[i]->getsize();
-	}
-
-	return total;
+    return total;
 }
 
-void Folder::deletefromdisk() {
-	try {
-		fs::remove_all(pathname);
-	}
-
-	catch (...) {
-		std::cout << "folder could not be deleted\n";
-	}
+bool Folder::deleteFromDisk() {
+    try {
+        if (fs::exists(diskpath)) {
+            fs::remove_all(diskpath);
+            return true;
+        }
+    }
+    catch (...) {
+        std::cout << "folder could not be deleted\n";
+    }
+    return false;
 }
 
 Folder::~Folder() {
-	for (int i = 0; i < count; i++) {
-		delete space[i];
-	}
+    for (int i = 0; i < count; i++) {
+        delete space[i];
+    }
 
-	delete[] space;
+    delete[] space;
 }
-
 
 void Folder::open() {
 }
