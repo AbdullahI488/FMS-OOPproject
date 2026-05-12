73f6c693e7c8abf0df237ebaeb0411d8d5fd6269
l250709 <l250709@lhr.nu.edu.pk>
2026-05-12 21:56:56 +0500

    Implement TxtFile class
    Expanded the TxtFile class to include methods for loading, saving, and editing text files, along with a vector to store lines.

--- src/TXTFile.h
+++ src/TXTFile.h
@@ -1,8 +1,27 @@
 #ifndef TXTFILE_H
 #define TXTFILE_H
 #include "Files.h"
-#include<iostream>
-class TXTFILE: public File {
-	
+#include <vector>
+#include <string>
+
+class TxtFile : public File {
+protected:
+    std::vector<std::string> lines;
+
+    void loadFromDisk() override;
+    void saveToDisk() override;
+    void runEditor() override;
+
+public:
+    TxtFile(const std::string& name,
+        const std::string& diskPath,
+        Folder* parent);
+
+    void createOnDisk() override;
+    void open() override;
+    std::string getType() const override { return "TXT"; }
+
+    bool deleteFromDisk() override;
 };
+
 #endif
