6d3ef624bff4001eaf008dc26ffe27f74819d4f2
AbdullahI488 <155844680+AbdullahI488@users.noreply.github.com>
2026-05-12 17:36:16 +0500

    Implement file size retrieval and deletion methods
    Added methods to get file size and delete file from disk.

--- src/Files.cpp
+++ src/Files.cpp
@@ -1,2 +1,33 @@
-#include "Files.h"
+#include "File.h"
+#include <filesystem>
+#include <iostream>
 
+namespace fs = std::filesystem;
+
+File::File(std::string name, std::string dp, Folder* np) : Node(name, path, ptr) {}
+
+long long File::getsize() const {
+	try {
+
+		if (fs::exists(pathname)) {
+			return (long long)fs::file_size(pathname);
+		}
+	}
+
+	catch (...) {}
+
+	return 0;
+}
+
+void File::deletefromdisk() {//actual file deletion
+
+	try {
+		if (fs::exists(pathname)) {
+			fs::remove(pathname);
+		}
+	}
+
+	catch (...) {
+		std::cout << "file could not be deleted\n";
+	}
+}
