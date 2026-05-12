885827da726d0e56181d8a2f6afa818f612178c5
AbdullahI488 <155844680+AbdullahI488@users.noreply.github.com>
2026-05-12 23:40:22 +0500

    Refactor Files class methods and variable names

--- src/Files.cpp
+++ src/Files.cpp
@@ -4,32 +4,25 @@
 
 namespace fs = std::filesystem;
 
-Files::Files(std::string name, std::string dp, Folder* np) : Node(name, path, ptr) {}
-
-long long Files::getsize() const {
-	try {
-
-		if (fs::exists(pathname)) {
-			return (long long)fs::file_size(pathname);
-		}
-	}
-
-	catch (...) {}
-
-	return 0;
+long long File::getsize() const {
+    try {
+        if (fs::exists(diskpath)) {
+            return static_cast<long long>(fs::file_size(diskpath));
+        }
+    }
+    catch (...) {}
+    return 0;
 }
 
-bool Files::deletefromdisk() {//actual file deletion
-
-	try {
-		if (fs::exists(pathname)) {
-			fs::remove(pathname);
-			return true;
-		}
-	}
-
-	catch (...) {
-		std::cout << "file could not be deleted\n";
-		return false;
-	}
+bool File::deleteFromDisk() {
+    try {
+        if (fs::exists(diskpath)) {
+            return fs::remove(diskpath);
+        }
+    }
+    catch (...) {
+        std::cout << "file could not be deleted\n";
+    }
+    return false;
 }
+
