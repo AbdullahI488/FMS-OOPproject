340135c61f75fa07a3f4b9fc8f4b3f184ebf3216
AbdullahI488 <155844680+AbdullahI488@users.noreply.github.com>
2026-05-12 16:05:41 +0500

    Expand Folder class with child management methods
    Added methods for managing child nodes and resizing.

--- src/Folder.h
+++ src/Folder.h
@@ -1,11 +1,46 @@
 #ifndef FOLDER_H
 #define FOLDER_H
-#include"Node.h"
-class Folder:public Node {
-    Node**space;
-    int count;
-    int capacity;
 
+#include "Node.h"
+
+class Folder : public Node {
+
+	Node** space;
+	int count;
+	int capacity;
+	void resize();
+
+public:
+
+	Folder(std::string name, std::string path, Folder* ptr);
+
+	~Folder();
+
+	void addchild(Node* node);
+
+	bool removechild(std::string name);
+
+	Node* findchild(std::string name) const;
+
+	std::string searchrecursive(std::string targetname) const;
+
+	int getcount() const {
+		return count;
+	}
+
+	Node* getchild(int i) const {
+		return space[i];
+	}
+
+	void open() override;
+
+	std::string gettype() const override {
+		return "Folder";
+	}
+
+	long long getsize() const override;
+
+	void deletefromdisk() override;
 };
-#endif
 
+#endif
