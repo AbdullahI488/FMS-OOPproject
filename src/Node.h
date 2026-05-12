8c38735529bead936c51caf4d4a02354c8b65b01
AbdullahI488 <155844680+AbdullahI488@users.noreply.github.com>
2026-05-12 13:52:24 +0500

    Refactor Node class structure and methods
    Refactor Node class with updated member variables and methods.

--- src/Node.h
+++ src/Node.h
@@ -1,22 +1,52 @@
 #ifndef NODE_H
 #define NODE_H
-#include<iostream>
+
+#include <string>
+#include <iostream>
+
 class Folder;
+
 class Node {
+
 protected:
-	std::string name;
-	std::string pathname;
-	Folder* ptr;
+
+    std::string name;
+    std::string diskpath;
+    Folder* parent; //it is a ptr to a parent so this var name makes more sense
+
 public:
-	Node(std::string n, std::string p, Folder* ptr);
-	std::string GetName();
-	std::string GetPath();
-	Folder* GetParent();
-	virtual ~Node();
-	virtual std::string GetType();
 
+    Node(const std::string& n, const std::string& dp, Folder* np); 
 
+    virtual ~Node() = default;
 
+    std::string getname() const {
+        return name;
+    }
+
+    std::string getdiskpath() const {
+        return diskpath;
+    }
+
+    Folder* getparent() const {
+        return parent;
+    }
+
+    void setname(const std::string& n) {
+        name = n;
+    }
+
+    void setdiskpath(const std::string& dp) {
+        diskpath = dp;
+    }
+
+    virtual void open() = 0; 
+
+    virtual std::string gettype() const = 0;
+
+    virtual long long getsize() const = 0;
+
+    virtual void deletefromdisk() = 0;
 };
 
 #endif
